99b7fcc48ceefcf6d62bd8b91e14710a763ba3b5
mmt817 <1669404371@qq.com>
2025-02-25 21:32:21 +0800

    [backend]:fix: - credentials凭证

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173',
+  credentials: true
 }))
 
 // 登录凭证路由挂载
